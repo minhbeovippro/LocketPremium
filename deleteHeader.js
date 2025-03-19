d80db7932d1574276f20f47d8f1f371d0885fe17
minhbeovippro <minhbeovippro3@gmail.com>
2025-03-20 01:10:25 +0700

    Update deleteHeader.js

--- deleteHeader.js
+++ deleteHeader.js
@@ -6,12 +6,5 @@ function setHeaderValue(e, a, d) {
 }
 
 var modifiedHeaders = $request.headers;
-
 setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
-
-setHeaderValue(modifiedHeaders, "X-Forwarded-For", "104.28.0.1");
-setHeaderValue(modifiedHeaders, "CF-Connecting-IP", "104.28.0.1");
-setHeaderValue(modifiedHeaders, "Accept-Language", "en-US,en;q=0.9");
-setHeaderValue(modifiedHeaders, "Geo-Location", "37.7749,-122.4194");
-
 $done({ headers: modifiedHeaders });
